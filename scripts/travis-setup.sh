a72ffb8b59fe1719af28f8ef8b53b5047d096608
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-03-21 15:47:22 -0600

    Start creating a git repo to build conda pkgs

---
+++ scripts/travis-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -x 
+set -e
+
+case $TRAVIS_OS_NAME in
+  linux)
+    osname=Linux
+  ;;
+  *)
+    osname=MacOSX
+esac
+
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-$osname-x86_64.sh
+sudo bash Miniconda3-latest-$osname-x86_64.sh -b -p /anaconda
+sudo chown -R $USER /anaconda
+export PATH=/anaconda/bin:$PATH
+
+# reversed order (preferred last)
+CHANNELS="conda-forge defaults r bioconda"
+
+for channel in $CHANNELS; do
+    conda config --add channels $channel
+done
+
+PACKAGES="
+anaconda-client
+conda
+conda-build
+
+
+conda index /anaconda/conda-bld/linux-64 /anaconda/conda-bld/osx-64
+conda config --add channels file://anaconda/conda-bld
