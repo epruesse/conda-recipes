5c05dfbffcb0ba3d0843e3a51b63adcc5ca5ea7b
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-03-21 18:58:21 -0600

    Workaround bug in conda/resolv.py

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -4,6 +4,9 @@ set -e
 
 export PATH=/anaconda/bin:$PATH
 
+# patch resolve.py to work around a bug
+patch /anaconda/lib/python3.6/site-packages/conda/resolve.py <scripts/resolve.patch
+
 conda-build-all recipes \
 		--inspect-channels epruesse \
 		--upload-channels epruesse \
