6c3488058144f7c727c58ece30fd73ac94f381bc
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-04-05 10:18:32 -0600

    Update ARB package

--- recipes/arb-bio/build.sh
+++ recipes/arb-bio/build.sh
@@ -6,44 +6,73 @@ export PATH=$ARBHOME/bin:$PATH
 export LD_LIBRARY_PATH=$ARBHOME/lib
 
 export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
-export XLIBS=$(shell pkg-config --libs xpm xerces-c)
-export XAW_LIBS=$(shell pkg-config --libs xaw7)
-export XML_INCLUDES=$(shell pkg-config --cflags xerces-c)
-export XINCLUDES=$(shell pkg-config --cflags x11)
+export XLIBS=$(pkg-config --libs xpm xerces-c)
+export XAW_LIBS=$(pkg-config --libs xaw7)
+export XML_INCLUDES=$(pkg-config --cflags xerces-c)
+export XINCLUDES=$(pkg-config --cflags x11)
 
 # Suppress building tools bundled with ARB for which we have
 # conda packages:
 export ARB_BUILD_SKIP_PKGS="MAFFT MUSCLE RAXML PHYLIP FASTTREE"
 
-if [ -z "$DIRTY" ]; then
+# ARB stores build settings in config.makefile. Create one from template:
+cp config.makefile.template config.makefile
 
-    make || true
+# Now add some target specific settings to config.makefile
+case `uname` in
+    Linux)
+	echo DARWIN := 0
+	echo LINUX := 1
+	echo MACH := LINUX
+	echo LINK_STATIC := 0
+	SHARED_LIB_SUFFIX=so
+	;;
+    Darwin)
+	echo DARWIN := 1
+	echo LINUX := 0
+	echo MACH := DARWIN
+	echo LINK_STATIC := 0
+	SHARED_LIB_SUFFIX=dylib
+	LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib"
+	;;
+esac >> config.makefile
 
-    case `uname` in
-	Linux)
-	    echo DARWIN := 0
-	    echo LINUX := 1
-	    echo MACH := LINUX
-	    ;;
-	Darwin)
-	    echo DARWIN := 1
-	    echo LINUX := 0
-	    echo MACH := DARWIN
-	    ;;
-    esac >> config.makefile
-fi
+# make
+make SHARED_LIB_SUFFIX=$SHARED_LIB_SUFFIX -j$CPU_COUNT build
 
-make_args="LIBPATH=-Wl,-rpath,$PREFIX/lib -L$ARBHOME/lib"
-no_mafftlinks="MAFFTLINKS="
+# make install
+make  SHARED_LIB_SUFFIX=$SHARED_LIB_SUFFIX  tarfile_quick
 
-make "$make_args" $no_mafftlinks -j$CPU_COUNT build >/dev/null 2>&1 || true
-make "$make_args" $no_mafftlinks -j1 build
-make "$make_args" $no_mafftlinks tarfile_quick
+ARB_INST=$PREFIX/lib/arb
+mkdir $ARB_INST
+tar -C $ARB_INST -xzf arb.tgz
+tar -C $ARB_INST -xzf arb-dev.tgz
 
-mkdir $PREFIX/lib/arb
-tar -C $PREFIX/lib/arb -xzf arb.tgz
-tar -C $PREFIX/lib/arb -xzf arb-dev.tgz
+(cd $PREFIX/bin; ln -s $ARB_INST/bin/arb)
 
-cd $PREFIX/bin
-ln -s $PREFIX/lib/arb/bin/arb
+case `uname` in
+    Darwin)
+	# fix library IDs
+	# ARB builds libraries as "-o ../lib/libXYZ.suf". That value becomes the
+	# "ID" of the lib and the path searched for by binaries. We need to
+	# change all these...
 
+	CHANGE_IDS=""
+	ARB_LIBS="$ARB_INST"/lib/*.$SHARED_LIB_SUFFIX
+	for lib in $ARB_LIBS; do
+	    old_id=`otool -D "$lib" | tail -n 1`
+	    new_id="@rpath/${lib##$PREFIX/lib/}"
+	    CHANGE_IDS="$CHANGE_IDS -change $old_id $new_id"
+	    install_name_tool -id "$new_id" "$lib"
+	    echo "Fixing ID of $lib ($old_id => $new_id)"
+	done
+
+	ARB_BINS=`find $ARB_INST -type f -perm -a=x | \
+	    xargs file | grep Mach-O | cut -d : -f 1 `
+
+	echo "Applying changes to binaries ($ARB_BINS)"
+	for bin in $ARB_BINS; do
+	    install_name_tool $CHANGE_IDS $bin
+	done
+	;;
+esac
