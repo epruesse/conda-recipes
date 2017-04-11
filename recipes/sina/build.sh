dc1c00f302a1c54821ad2817fb6b9fb214a2a513
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-04-11 12:52:19 -0600

    Update SINA to 1.3.1-a5

--- recipes/sina/build.sh
+++ recipes/sina/build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-rm $PREFIX/bin/sed
-
 # Get ARBHOME:
 ARBHOME=`echo | arb shell | sed -n 's/.*ARBHOME=.\(.*\).$/\1/p'`
-export ARBHOME
 
-bash autogen.sh
+#sh autogen.sh
 ./configure --prefix=$PREFIX \
 	    --disable-dependency-tracking \
 	    --disable-silent-rules \
 	    --with-sysroot=$PREFIX \
-	    --with-arbhome=$ARBHOME ||
+	    --with-boost=$PREFIX \
+	    --with-arbhome=$ARBHOME \
+            --disable-docs
 
-bash --norc
 make -j$(CPU_COUNT)
-make check || true
+make check
 make install
+
+rm -rf $PREFIX/share/doc/sina
